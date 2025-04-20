2353b060025688bde44bb92f3a113aa77da8f9fa
Boe Zhou <55188595+boezzz@users.noreply.github.com>
2025-04-19 22:10:08 -0700

    Initial Commit

---
+++ motion_data_app/motion_data_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  motion_data_appApp.swift
+//  motion_data_app
+//
+//  Created by Macintosh on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct motion_data_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ motion_data_appTests/motion_data_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  motion_data_appTests.swift
+//  motion_data_appTests
+//
+//  Created by Macintosh on 4/19/25.
+//
+
+import Testing
+@testable import motion_data_app
+
+struct motion_data_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
